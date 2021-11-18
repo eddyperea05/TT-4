3c02a25bcdc37124353d5f533ab27c08d4cde71b
eddyperea05 <eddyperea05@gmail.com>
2021-11-17 22:09:07 -0500

    Entrega 4

---
+++ README.md
@@ -0,0 +1,4 @@
+En el punto 6 se encuentran el punto 6 y 7
+
+Realizado por   Eddy Perea
+                Juan Carlos Alvarez
--- punto6.py
+++ punto6.py
@@ -3,9 +3,9 @@ randnums = np.random.randint(0, 9, size=(3, 3, 3))
 print(randnums)
 #Punto 7
 
-
-print(np.where(randnums == np.amin(randnums)))
-print(np.amin(randnums))
-print(np.where(randnums == np.amax(randnums)))
-print(np.amax(randnums))
-#print(np.argmax(randnums))
+#print(np.where(randnums == np.amin(randnums)))
+#print(np.amin(randnums))
+#print(np.where(randnums == np.amax(randnums)))
+#print(np.amax(randnums))
+print(np.argmin(randnums))
+print(np.argmax(randnums))
